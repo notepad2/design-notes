9df346858b64ba69ce1e4036378bcaf996e05a91
notepad2 <notepad2@live.cn>
2019-12-29 21:35:12 +0800

    20191229

--- design-notes/Form1.vb
+++ design-notes/Form1.vb
@@ -1,3 +1,30 @@
-﻿Public Class Form1
+﻿Imports Microsoft.Office.Interop '声明1
+Imports Microsoft.Office.Interop.Word '声明2
+Public Class Form1
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        Dim MyWord As Word.Application
+        Dim MyDoc As Word.Document
+        Dim table1 As Word.Table
+        Dim para1 As Word.Paragraph
+        Dim para2 As Word.Paragraph
+        Dim para3 As Word.Paragraph
+        Dim MyRange As Word.Range
 
+
+        MyWord = CreateObject("word.application")
+
+        MyDoc = MyWord.Documents.Add
+        'MyDoc.PageSetup.
+        MyRange = MyDoc.Range(Start:=0, End:=0)
+
+        MyWord.Visible = True
+        MyDoc.Activate()
+
+
+
+    End Sub
+
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+
+    End Sub
 End Class
