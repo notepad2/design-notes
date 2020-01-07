f35d96534c6aadcd4b27439f228ae79386a101c4
notepad2 <notepad2@live.cn>
2020-01-07 19:09:39 +0800

    自动编号之前

--- design-notes/Form1.vb
+++ design-notes/Form1.vb
@@ -8,6 +8,8 @@ Public Class Form1
 
     Dim CalcBook As Word.Document
 
+    Dim ActiveRange As Word.Range
+
     Dim table(100) As Word.Table
 
     Dim para(100) As Word.Paragraph
@@ -51,7 +53,6 @@ Public Class Form1
 
     Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
 
-
         '启动word
         Word1 = CreateObject("word.application")
 
@@ -60,7 +61,8 @@ Public Class Form1
 
         'para2 = "道路等级：桥面按非机动车道设计。"
 
-        '文档激活
+        '文档激活，可见
+        Word1.Visible = True
         CalcBook.Activate()
 
         '页面设置采用A3，横向，分两栏
@@ -81,8 +83,55 @@ Public Class Form1
         '.CharacterUnitFirstLineIndent = 2
         'End With
 
+        ActiveRange = CalcBook.Range(Start:=0, [End]:=0)
 
-        CalcBook.Content.InsertAfter(BridgeName)
+        'CalcBook.Content.InsertAfter("本次设计" & BridgeName & "位于" & Place & "，跨越" & RiverName & "，桥梁中心线与河道中心线成" & Angle)
+
+        '第一章，工程概况
+        ActiveRange.Style = CalcBook.Styles("标题 1")
+        ActiveRange.InsertAfter("工程概况" & Chr(13))
+        ActiveRange.Start = ActiveRange.End
+
+        ActiveRange.Style = CalcBook.Styles("正文")
+        ActiveRange.InsertAfter("本次设计" & BridgeName & "位于" & Place & "，跨越" & RiverName & "，桥梁中心线与河道中心线成" & Angle & Chr(13))
+        ActiveRange.Start = ActiveRange.End
+
+        '第二章，设计规范及依据
+        ActiveRange.Style = CalcBook.Styles("标题 1")
+        ActiveRange.InsertAfter("设计规范及依据" & Chr(13))
+        ActiveRange.Start = ActiveRange.End
+
+        ActiveRange.Style = CalcBook.Styles("正文")
+        ActiveRange.InsertAfter("规范内容" & Chr(13))
+        ActiveRange.Start = ActiveRange.End
+
+        '第三章，批复执行情况
+        ActiveRange.Style = CalcBook.Styles("标题 1")
+        ActiveRange.InsertAfter("初步设计批复意见执行情况" & Chr(13))
+        ActiveRange.Start = ActiveRange.End
+
+        ActiveRange.Style = CalcBook.Styles("正文")
+        ActiveRange.InsertAfter("批复内容及回复" & Chr(13))
+        ActiveRange.Start = ActiveRange.End
+
+        '第四章，工程地质
+        ActiveRange.Style = CalcBook.Styles("标题 1")
+        ActiveRange.InsertAfter("工程地质" & Chr(13))
+        ActiveRange.Start = ActiveRange.End
+
+        ActiveRange.Style = CalcBook.Styles("正文")
+        ActiveRange.InsertAfter("土层描述内容" & Chr(13))
+        ActiveRange.Start = ActiveRange.End
+
+        '第五章，主要技术标准
+        ActiveRange.Style = CalcBook.Styles("标题 1")
+        ActiveRange.InsertAfter("主要技术标准" & Chr(13))
+        ActiveRange.Start = ActiveRange.End
+
+        ActiveRange.Style = CalcBook.Styles("正文")
+        ActiveRange.InsertAfter("土层描述内容" & Chr(13))
+        ActiveRange.Start = ActiveRange.End
+
 
     End Sub
 
